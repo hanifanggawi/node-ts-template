eb24547c2d4a0f24d2b1ef98b5678fc9e4ac227f
hanifanggawi <hanifanggawi45@gmail.com>
2022-06-04 18:27:10 +0700

    refactor: format index file

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,14 @@ import { requestLogger } from "./util/middleware";
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 8000
 const HOST = process.env.HOST || 'http://localhost'
+const PORT = process.env.PORT || 8000
 
-app.use(express.urlencoded({ extended: true }))
+// Middlewares
 app.use(express.json())
 app.use(requestLogger)
 
+// Routes
 app.use('/', defaultRouter())
 
 app.listen(PORT, () => console.log(`Server started on ${HOST}:${PORT}`))
