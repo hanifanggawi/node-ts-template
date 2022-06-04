3aed2a89ab32d5c1819bc8f8bdafa2bda03f362c
hanifanggawi <hanifanggawi45@gmail.com>
2022-06-04 18:22:58 +0700

    feat: add HOST env

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 8000
-const HOST = process.env.PORT || 'http://localhost'
+const HOST = process.env.HOST || 'http://localhost'
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
